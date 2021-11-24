56b4709b23a7b19740345bae2ad5ef68daab555c
Heikki Kilpeläinen <heikki.kilpelainen@metropolia.fi>
2021-11-24 14:09:23 +0200

    Fix linter errors

--- controller/keyboard.py
+++ controller/keyboard.py
@@ -1,12 +1,12 @@
 # Keyboard demo
 
 import sys
-import serial
-import RPi.GPIO as GPIO
 from subprocess import Popen
 from time import sleep
 from os import path
 from os import walk
+import serial
+import RPi.GPIO as GPIO
 
 # Rpi e-ink hat switch BCM pins
 SW1 = "21"
@@ -16,7 +16,7 @@ keys = {\
         "KEY_OEM_3":SW1,\
         "KEY_LSHIFT":SW2\
         }
-keycodes = {\
+scancodes = {\
         "KEY_OEM_3":"\x00\x33" #TODO real code
         }
 # UART device (RDX = GPIO 14, TDX = GPIO 15)
@@ -41,25 +41,29 @@ def update_key(img_path):
     exe="/home/pi/keyboard_firmware/bin/edp.exe"
     # Print each found image into its correspoding display
     images = next(walk(img_path), (None, None, []))[2]
-    for img in images:
+    for i in images:
         try:
-            if keys[img]:
+            if keys[i]:
             # here we would select the correct key, but we got only one key.
                 cmd=f"{exe} {img}"
         except KeyError:
             continue
     try:
         Popen(["/bin/bash", cmd])
-    except:
-        print("Error: Could not update key (subprocess popen). Exit.")
+    except subprocess.CalledProcessError as e:
+        print("Error: Could not update key subprocess popen.")
+        print(e)
+        print("Exit.")
         sys.exit()
 
 # Send switch event with keycode to host with UART
 def send_key(scancode):
     try:
         UART.write(scancode)
-    except:
-        print("Error: Could not send to serial. Exit.")
+    except SerialException as e:
+        print("Error: Could not send to serial.")
+        print(e)
+        print("Exit.")
         sys.exit()
 
 # Prioritize key image update over transmission
@@ -75,19 +79,23 @@ if __name__ == "__main__":
         if img != "":
             update_key(img)
     # Error
-    except:
-        print("Error: Serial could not read. Exit.")
+    except SerialException as e:
+        print("Error: Serial could not read.")
+        print(e)
+        print("Exit.")
         sys.exit()
     #TODO test if serial flushing is needed as "finally"
 
     # Poll switch 1, here we would listen to or poll the whole matrix, but no.
     try:
         while True:
-            if GPIO.input(SW1):
-                send_key(scancode)
-                pass
+            for key in keys:
+                if GPIO.input(keys[key]):
+                    send_key(scancodes[key])
             sleep(0.1)
     # Error
-    except:
-        print(f"Error: GPIO {SW1} could not be read. Exit.")
+    except Exception as e:
+        print(f"Error: GPIO {SW1} could not be read.")
+        print(e)
+        print("Exit.")
         sys.exit()
